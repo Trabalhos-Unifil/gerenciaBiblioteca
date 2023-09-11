298dc7dcc4201669d72a8fa9acb3d12ef60be4ac
carlos saels <carlos.sales@edu.unifil.br>
2023-09-11 19:09:24 -0300

    ClasseClientePessoa

--- src/Cliente.java
+++ src/Cliente.java
@@ -1,11 +1,26 @@
 public class Cliente extends Pessoa {
-
-    private int inscricao;
-
+    private boolean inscrito;
 
     public Cliente(String nome, String cpf) {
         super(nome, cpf);
+        this.inscrito = false;
+
+        // O cliente não está incrito
+    }
+
+    public void fazerInscricao() {
+        if (inscrito) {
+            inscrito = true;
+            System.out.println("Cliente " + getNome() + " Inscrição realizada com sucesso.");
+        } else {
+            System.out.println("Cliente " + getNome() + " Já está inscrito.");
+        }
+    }
+
+    public static void main(String[] args) {
+        Cliente cliente1 = new Cliente("Giovana", "117.929.789-01");
+
+        cliente1.fazerInscricao(); // Cliente faz a inscrição pela primeira vez
+        cliente1.fazerInscricao(); // Tentativa de fazer a inscrição novamente
     }
 }
-
-
